5f4c325139092b0bf5a4430c537f09a3ea736ad0
Tim Caswell <tim@creationix.com>
2012-09-10 10:06:05 -0700

    Add basic smoke-screen test

---
+++ test/test-ssh.js
@@ -0,0 +1,14 @@
+var Master = require('vfs-ssh').Master;
+
+console.log(Master);
+
+var master = new Master({
+  root: "/home/tim/",
+  host: "creationix.com",
+  nodePath: "/home/tim/nvm/v0.8.4/bin/node"
+});
+
+master.connect(function (err, vfs) {
+  if (err) throw err;
+  console.log(vfs);
+});
